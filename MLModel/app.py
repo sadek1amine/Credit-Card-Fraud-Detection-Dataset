dc3bb27cbdf024a0608be942fd5ca44b1bfe4040
sadek1amine <148018682+sadek1amine@users.noreply.github.com>
2026-07-11 23:59:29 +0100

    Update app.py

--- MLModel/app.py
+++ MLModel/app.py
@@ -8,7 +8,7 @@ from typing import List
 class Transaction(BaseModel):
     features: List[float] = Field(..., min_items=30, max_items=30)
 
-app = FastAPI(title="💸 Fraud Detection API")
+app = FastAPI(title="💸 Fraud Detection API")GGGGGGGGGGGGGGGGGGGGG
 
 # تحميل الملفات المحفوظة
 try:
