952b859ac208b5c7c21a263975f116fa8d011e15
sadek1amine <148018682+sadek1amine@users.noreply.github.com>
2026-07-11 23:57:27 +0100

    Update fraud_detection_model.py

--- MLModel/fraud_detection_model.py
+++ MLModel/fraud_detection_model.py
@@ -13,7 +13,7 @@ from sklearn.preprocessing import StandardScaler
 # 📦 1. Load Dataset
 # =========================================
 
-print("📥 Loading dataset...")
+print("📥 Loading dataset...")hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
 # Make sure creditcard.csv is in the same directory
 data = pd.read_csv("creditcard.csv")
 
