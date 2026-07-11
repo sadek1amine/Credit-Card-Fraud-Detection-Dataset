9da15654c787faeae14e9332c913582eeebbb32c
sadek1amine <148018682+sadek1amine@users.noreply.github.com>
2026-07-11 23:57:07 +0100

    Update app.py

--- MLModel/app.py
+++ MLModel/app.py
@@ -2,7 +2,7 @@ import pickle
 import numpy as np
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
-from typing import Listgggggggggggggggggggggggggggggggggggg
+from typing import List
 
 # 1. بناء الهيكل المدخل
 class Transaction(BaseModel):
